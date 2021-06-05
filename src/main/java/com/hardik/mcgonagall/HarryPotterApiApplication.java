5f6d4480572ea8ee7b774a139c18c48e9e992704
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-05 16:11:13 +0530

    initial dummy project commit

---
+++ src/main/java/com/hardik/mcgonagall/HarryPotterApiApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.mcgonagall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarryPotterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarryPotterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/mcgonagall/HarryPotterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.mcgonagall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarryPotterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
